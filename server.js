24a588aa2ba75b74c988d6d4be8ba2db76df2c21
arifoyong <arifoyong@gmail.com>
2018-09-09 12:12:43 +0800

    test app.use directory

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 const app = express();
 
+console.log(process.env);
+console.log(express.static(path.join(__dirname, 'build')));
+console.log(path.join(__dirname, 'build', 'index.html'));
+
 // serve static asset normally
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // handle all routes
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/build', '/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
 
